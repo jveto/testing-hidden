bab6af1dd9a0741fc225470fc7f9aee7b705bc5d
Jeremy Veto <jveto91@gmail.comgit config --global user.name jvetogit commit -m testing_first_commitlssotpstopgithelp--global user.email jveto91@gmail.com>
2018-07-08 20:21:01 -0500

    updating files

--- server.py
+++ server.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
 app.secret_key="notouchy"
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
-    session['visits'] += 1
-    print("************************************ we got it")
-    print(session['visits'])
-    if request.form['action'] == "2":
-        print("gotit")
+    if not session['visits']:
+        session['visits'] = 1
     return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/twoVisits', methods = ['POST'])
+def twoVisits():
+    if request.form['visits2']:
+        session['visits'] += int(request.form['visits2'])
+    return redirect('/')
+
+@app.route('/zeroVisits', methods=['POST'])
+def zeroVisits():
+    if request.form['visits0']:
+        session['visits'] = int(request.form['visits0'])
+    return redirect('/')
+
+
+if __name__ == "__main__":  
     app.run(debug = True)
